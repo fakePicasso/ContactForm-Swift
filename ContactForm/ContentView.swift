8faa698fd19cbba187d426a62824bc821bd468e2
contactatfp <85082344+contactatfp@users.noreply.github.com>
2021-08-31 09:23:01 -0700

    Contact Form Swift UI

--- ContactForm/ContentView.swift
+++ ContactForm/ContentView.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var emailAddress = ""
+  @State private var phoneNumber = ""
+  @State private var message = "Write your message here..."
+  @State private var subject = "Help"
+  @State private var includeLogs = false
+ 
+  @State private var preferredContactMethod = ContactMethod.email
+ 
+  enum ContactMethod {
+    case email, phone
+  }
+ 
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("How can we reach you?")) {
+          Picker("Contact Method", selection: $preferredContactMethod) {
+            Text("Email").tag(ContactMethod.email)
+            Text("Phone").tag(ContactMethod.phone)
+          }
+          .pickerStyle(SegmentedPickerStyle())
+          switch preferredContactMethod {
+          case .email:
+            TextField("Email Address", text: $emailAddress)
+          case .phone:
+            TextField("Phone Number", text: $phoneNumber)
+          }
+        }
+        Section(header: Text("What can we help you with?")) {
+          Picker("Subject", selection: $subject) {
+            ForEach(["Help", "Suggestion", "Feature Request"], id: \.self) { subject in
+              Text(subject)
+            }
+          }
+        }
+        Section(header: Text("Briefly explain what‘s going on.")) {
+            TextEditor(text: $message)
+        }
+        Section(footer: Text("This information will be sent anonymously.")) {
+          Toggle("Include Logs", isOn: $includeLogs)
+        }
+        Button("Submit", action: {
+          print("Submit button tapped")
+        })
+      }
+      .navigationTitle("Contact Us")
+      
     }
+  }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
